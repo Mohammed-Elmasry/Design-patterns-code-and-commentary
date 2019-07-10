4b81e7e21593aa9b2316b277ec664bdba0363c52
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-10 23:03:08 +0200

    define a Duck base class

---
+++ strategy_pattern/ducks/Duck.py
@@ -0,0 +1,13 @@
+# this is the definition of the Duck class
+
+
+class Duck:
+
+    quack_behavior = None
+    fly_behavior = None
+
+    def fly(self):
+        pass
+
+    def quack(self):
+        pass
