fb0c11358c7ff37b0039854c386a388c0d6344a1
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-11 01:10:11 +0200

    test the new setter methods with different behaviors

--- strategy_pattern/main.py
+++ strategy_pattern/main.py
@@ -2,6 +2,7 @@
 
 from strategy_pattern.ducks.RubberDuck import RubberDucky
 from strategy_pattern.behaviors.concrete_behaviors.FlyRocketPower import FlyRocketPowered
+from strategy_pattern.behaviors.concrete_behaviors.NoFly import FlyNoWay
 from strategy_pattern.behaviors.concrete_behaviors.Skweal import Skweal
 
 # declare behaviors
@@ -13,4 +14,13 @@ myRubberDuck = RubberDucky(fly_rocket, skweal_behavior)
 myRubberDuck.fly()
 myRubberDuck.quack()
 
+no_fly = FlyNoWay()
+
+# create another kind of flying behavior and assign it to the rubber duck
+
+myRubberDuck.set_fly_behavior(no_fly)
+myRubberDuck.fly()
+myRubberDuck.quack()
+
+# create another kind of quack behavior and assign it to the rubber duck
 
