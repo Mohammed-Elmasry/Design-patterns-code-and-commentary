0e365bd6bfc5e7951939001166ceb9c4727d2444
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-10 23:28:41 +0200

    define __init__.py in duck directory

--- strategy_pattern/main.py
+++ strategy_pattern/main.py
@@ -1,2 +1,11 @@
 # this is the main module that'll run the strategy design pattern demo
 
+from .ducks.RubberDuck import *
+from .behaviors.concrete_behaviors import FlyRocketPower, FlyWithWings, NoFly
+
+# create a behavior
+fly_rocket = FlyRocketPower()
+
+# create a rubber duck and change its behaviors
+rubberDuck = RubberDuck(fly_rocket)
+rubberDuck.fly()
