5d7c32c58c3461ea8bbed4f13b2dab756f8e34c5
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-10 23:07:00 +0200

    define the code that uses the behavior objects (before creating them)

--- strategy_pattern/ducks/RubberDuck.py
+++ strategy_pattern/ducks/RubberDuck.py
@@ -4,7 +4,15 @@ from . import Duck
 
 class RubberDuck(Duck):
 
+    def __init__(self, fly_bahvior, quack_behavior):
+        self.quack_behavior = quack_behavior
+        self.fly_behavior = fly_bahvior
+
     quack_behavior = None
     fly_behavior = None
 
-    
+    def fly(self):
+        self.fly_behavior.fly()
+
+    def quack(self):
+        self.quack_behavior.quack()
